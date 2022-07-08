99d1bc38cc3639171615aab25716dc34128a3eee
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-08 20:14:54 +0300

    Dto

---
+++ src/main/java/com/odeyalo/support/clients/auth/dto/PhoneNumberMethodPasswordRecoveryDTO.java
@@ -0,0 +1,13 @@
+package com.odeyalo.support.clients.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PhoneNumberMethodPasswordRecoveryDTO {
+    private String phoneNumber;
+
+}
