757c733ce4ef0e81d20496bb43a1b5eb610f2aff
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-08 20:14:51 +0300

    Dto

---
+++ src/main/java/com/odeyalo/support/clients/auth/dto/JwtTokenResponseDTO.java
@@ -0,0 +1,14 @@
+package com.odeyalo.support.clients.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtTokenResponseDTO {
+    private boolean isSuccess;
+    private String jwtToken;
+    private String refreshToken;
+}
