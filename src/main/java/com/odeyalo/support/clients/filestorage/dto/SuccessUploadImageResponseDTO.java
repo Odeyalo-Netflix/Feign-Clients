9a25bb305170943473ae342b97d5e62fe39e7a1f
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-21 17:29:16 +0300

    Contains id if upload was success

---
+++ src/main/java/com/odeyalo/support/clients/filestorage/dto/SuccessUploadImageResponseDTO.java
@@ -0,0 +1,14 @@
+package com.odeyalo.support.clients.filestorage.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SuccessUploadImageResponseDTO {
+    private String id;
+}
