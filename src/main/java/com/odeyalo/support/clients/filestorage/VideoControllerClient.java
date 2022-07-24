1d22f24600965253727ee07d04ce4f7597bceef2
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-24 14:51:23 +0300

    Added configuration to client

--- src/main/java/com/odeyalo/support/clients/filestorage/VideoControllerClient.java
+++ src/main/java/com/odeyalo/support/clients/filestorage/VideoControllerClient.java
@@ -1,16 +1,19 @@
 package com.odeyalo.support.clients.filestorage;
 
+import com.odeyalo.support.clients.configuration.FileUploadFeignClientConfiguration;
 import com.odeyalo.support.clients.filestorage.dto.SuccessUploadVideoResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 @FeignClient(name = "video-controller-client",
-        url = "${app.clients.filestorage.video.urls.domain}")
+        url = "${app.clients.filestorage.video.urls.domain}",
+        configuration = FileUploadFeignClientConfiguration.class)
 public interface VideoControllerClient {
 
-    @PostMapping("/upload")
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     ResponseEntity<SuccessUploadVideoResponseDTO> saveVideo(@RequestPart(name = "video") MultipartFile file);
 }
