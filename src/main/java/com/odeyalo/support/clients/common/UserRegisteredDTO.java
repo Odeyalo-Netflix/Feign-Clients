01162c144f7c9d40e4949f9f63a4c91140eaf73e
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-03 15:35:51 +0300

    Contains a main information about registered user

---
+++ src/main/java/com/odeyalo/support/clients/common/UserRegisteredDTO.java
@@ -0,0 +1,14 @@
+package com.odeyalo.support.clients.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserRegisteredDTO {
+    private UserInformationDTO info;
+}
