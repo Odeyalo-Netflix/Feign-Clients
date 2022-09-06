77701e05a8143d3f3c112810e45c901d318dc52e
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-06 10:15:10 +0300

    Added empty constructor and all args constructor

--- src/main/java/com/odeyalo/support/clients/notification/dto/TemplatedEmailMessageDTO.java
+++ src/main/java/com/odeyalo/support/clients/notification/dto/TemplatedEmailMessageDTO.java
@@ -1,10 +1,14 @@
 package com.odeyalo.support.clients.notification.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TemplatedEmailMessageDTO {
     private String to;
     private String subject;
