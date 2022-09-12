f2976d92fd11b2ed3a829e567d917a3574e775a8
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-12 17:21:15 +0300

    Moved from TemplatedEmailMessageDTO, added VERIFICATION_CODE_EMAIL_LETTER constant

---
+++ src/main/java/com/odeyalo/support/clients/notification/dto/TemplateTypeNameConstants.java
@@ -0,0 +1,6 @@
+package com.odeyalo.support.clients.notification.dto;
+
+public class TemplateTypeNameConstants {
+    public static final String PREMIUM_MEMBER_EMAIL_LETTER = "PREMIUM_MEMBER_EMAIL_LETTER";
+    public static final String VERIFICATION_CODE_EMAIL_LETTER = "CODE_VERIFICATION_EMAIL_LETTER_TEMPLATE";
+}
