048c6f11cba11b1fc88c36e6c9b78225c44116bb
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-12 17:20:45 +0300

    Deleted TemplateTypeNameConstants inner class

--- src/main/java/com/odeyalo/support/clients/notification/dto/TemplatedEmailMessageDTO.java
+++ src/main/java/com/odeyalo/support/clients/notification/dto/TemplatedEmailMessageDTO.java
@@ -13,8 +13,4 @@ public class TemplatedEmailMessageDTO {
     private String to;
     private String subject;
     private String templateType;
-
-    public static class TemplateTypeNameConstants {
-        public static final String PREMIUM_MEMBER_EMAIL_LETTER = "PREMIUM_MEMBER_EMAIL_LETTER";
-    }
 }
