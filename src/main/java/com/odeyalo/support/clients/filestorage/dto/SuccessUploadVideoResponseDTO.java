d24f3758f9f184477770551277910a530509e274
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-14 14:37:50 +0300

    Added links

--- src/main/java/com/odeyalo/support/clients/filestorage/dto/SuccessUploadVideoResponseDTO.java
+++ src/main/java/com/odeyalo/support/clients/filestorage/dto/SuccessUploadVideoResponseDTO.java
@@ -5,10 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class SuccessUploadVideoResponseDTO {
     private String videoId;
+    private List<LinkResponse> links;
 }
